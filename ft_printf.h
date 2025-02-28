1994bbd3a381f21ac071415d3a59be7f6451f644
sargevor <sargis.gevorgyan2.y@gmail.com>
2025-02-28 16:17:02 +0400

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -16,3 +16,13 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_printf(const char *s, ...);
+int	ft_choose(const char *s, va_list list);
+int	ft_check(char c);
+int	ft_putnbr_base(unsigned int nbr, char *simb, unsigned int base);
+int	ft_signed(long long nbr);
+int	ft_putptr(size_t nbr, char *simb, size_t base);
+
+#endif
