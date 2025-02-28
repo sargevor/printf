8e1a1da8705c8b0c61ec535959d9d72546d27bf8
sargevor <sargis.gevorgyan2.y@gmail.com>
2025-02-28 16:16:31 +0400

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -12,7 +12,30 @@
 
 #include "ft_printf.h"
 
-int ft_printf(const char *prt, ...)
+int ft_printf(const char *s, ...)
 {
-    
+    va_list	list;
+	int		len;
+
+	len = 0;
+	va_start(list, s);
+	while (*s)
+	{
+		if (*s == '%')
+		{
+			s++;
+			if (ft_check(*s))
+			{
+				len += ft_choose(s++, list);
+			}
+		}
+		if (*s != '%' && *s)
+		{
+			len += ft_putchar(*s);
+			s++;
+		}
+	}
+	va_end(list);
+	return (len);
+
 }
